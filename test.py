dfdf075ff11d4ab05fa8f30de3821ffd24fb7f8b
Max <maxjapri@gmail.com>
2024-08-23 15:34:37 +0700

    coba

--- test.py
+++ test.py
@@ -1,11 +1,6 @@
 import requests
-import json
+import base64
 
-body = {
-  "pageSize": 200
-}
-response = requests.post('https://api.gms.moontontech.com/api/gms/source/2669606/2681577', json=body)
-data = json.loads(response.text)
-
-for d in data['data']['records']:
-  print(d['data']['name'])
+link = 'aHR0cHM6Ly9oYWhvLm1vZQ=='
+test = base64.b64decode(link)
+requests.get(test).text
